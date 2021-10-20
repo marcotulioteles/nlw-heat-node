2280a8040d2842e936545772ebca8ad2e0ff9613
Marco Túlio Teles <marcotuliocivileng@gmail.com>
2021-10-20 18:39:44 -0300

    builded get last 3 msg and create userprofile

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastThreeMessagesController } from "./controllers/GetLastThreeMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
 const router = Router();
@@ -11,5 +13,11 @@ router.post(
   ensureAuthenticated,
   new CreateMessageController().handle
 );
+router.get("/messages/last3", new GetLastThreeMessagesController().handle);
+router.get(
+  "/profile",
+  ensureAuthenticated,
+  new ProfileUserController().handle
+)
 
 export { router }
